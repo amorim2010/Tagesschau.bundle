aaedcc3985b94d0900c260fcc99d7c97b09a98b3
sander1 <sander@plexapp.com>
2013-02-07 13:22:22 +0100

    Update channel to 2.1 framework.

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,32 +1,55 @@
-# Code by Tom Rothe (motine)
-
-NAME = "Tagesschau"
-ART = "art-default.jpg"
-ICON = "icon-default.png"
+NAME = 'Tagesschau'
+ART = 'art-default.jpg'
+ICON = 'icon-default.png'
 
 ####################################################################################################
-
 def Start():
-  Plugin.AddPrefixHandler('/video/tagesschau', VideoMainMenu, NAME, ICON, ART)
-  Plugin.AddViewGroup("InfoList", viewMode="InfoList", mediaType="items")
 
-  MediaContainer.art = R(ART)
-  MediaContainer.title = NAME
-  MediaContainer.viewGroup = "InfoList"
-  DirectoryItem.thumb = R(ICON)
-  VideoItem.thumb = R(ICON)
-  WebVideoItem.thumb = R(ICON)
+	ObjectContainer.art = R(ART)
+	ObjectContainer.title1 = NAME
+	VideoClipObject.thumb = R(ICON)
 
 ####################################################################################################
+@handler('/video/tagesschau', NAME, thumb=ICON, art=ART)
+def MainMenu():
 
-def VideoMainMenu():
-  dir = MediaContainer()
-  for item in XML.ElementFromURL('http://www.tagesschau.de/export/video-podcast/webl/tagesschau', errors='ignore').xpath('//item'):
-    title       = item.find('title').text.strip()
-    date        = item.find('pubDate').text.strip()
-    description = item.find('description').text.strip()
-    url         = item.find('enclosure').get('url').strip()
+	oc = ObjectContainer()
 
-    dir.Append(VideoItem(url, title=title, subtitle=date, summary=description))
+	for item in XML.ElementFromURL('http://www.tagesschau.de/export/video-podcast/webl/tagesschau').xpath('//item'):
+		url = item.xpath('./enclosure/@url')[0]
+		title = item.xpath('./title/text()')[0]
+		summary = item.xpath('./description/text()')[0]
+		originally_available_at = Datetime.ParseDate(item.xpath('./pubDate/text()')[0])
 
-  return dir
+		oc.add(CreateVideoClipObject(url=url, title=title, summary=summary, originally_available_at=originally_available_at))
+
+	return oc
+
+####################################################################################################
+def CreateVideoClipObject(url, title, summary, originally_available_at, include_container=False):
+
+	videoclip_obj = VideoClipObject(
+		key = Callback(CreateVideoClipObject, url=url, title=title, summary=summary, originally_available_at=originally_available_at, include_container=True),
+		rating_key = url,
+		title = title,
+		summary = summary,
+		originally_available_at = originally_available_at,
+		items = [
+			MediaObject(
+				parts = [
+					PartObject(key=url)
+				],
+				container = Container.MP4,
+				video_codec = VideoCodec.H264,
+				video_resolution = '544',
+				audio_codec = AudioCodec.AAC,
+				audio_channels = 2,
+				optimized_for_streaming = True
+			)
+		]
+	)
+
+	if include_container:
+		return ObjectContainer(objects=[videoclip_obj])
+	else:
+		return videoclip_obj
